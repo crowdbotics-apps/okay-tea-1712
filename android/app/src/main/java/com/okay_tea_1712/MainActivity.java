ae74ac4ed71ad7a9fb93c46c01b8ab1765e7cb86
Crowdbotics <team@crowdbotics.com>
2019-03-28 10:48:53 +0000

    initial commit

---
+++ android/app/src/main/java/com/okay_tea_1712/MainActivity.java
@@ -0,0 +1,15 @@
+package com.okay_tea_1712;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "okay_tea_1712";
+    }
+}
